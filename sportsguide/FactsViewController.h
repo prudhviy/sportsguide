031d9fc4350aab4a4ad6f894045fc5ebb953c420
Bhaskar teja Yerneni <prudhviy@gmail.com>
2015-05-26 18:44:18 -0400

    added facts view controller

---
+++ sportsguide/FactsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FactsViewController.h
+//  sportsguide
+//
+//  Created by prudhvi on 5/26/15.
+//  Copyright (c) 2015 gits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FactsViewController : UITableViewController
+
+@end
